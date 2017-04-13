77df4ada6d1b44dab3185812c7ab940c50f74e3c
Deepanshibajaj1503 <deepanshi1503@gmail.com>
2017-04-13 12:41:11 +0530

    app flavors

--- app/src/main/java/in/sportscafe/nostragamus/module/allchallenges/info/ChallengeConfigsApiModelImpl.java
+++ app/src/main/java/in/sportscafe/nostragamus/module/allchallenges/info/ChallengeConfigsApiModelImpl.java
@@ -1,9 +1,12 @@
 package in.sportscafe.nostragamus.module.allchallenges.info;
 
+import com.jeeva.android.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import in.sportscafe.nostragamus.BuildConfig;
 import in.sportscafe.nostragamus.Nostragamus;
 import in.sportscafe.nostragamus.module.allchallenges.dto.ChallengeConfig;
 import in.sportscafe.nostragamus.module.allchallenges.dto.ChallengeConfigsResponse;
@@ -27,18 +30,22 @@ public class ChallengeConfigsApiModelImpl {
         return new ChallengeConfigsApiModelImpl(listener);
     }
 
-    public void getConfigs(int challengeId,Integer configIndex) {
+    public void getConfigs(int challengeId, Integer configIndex) {
         if (Nostragamus.getInstance().hasNetworkConnection()) {
-            callConfigsApi(challengeId,configIndex);
+            callConfigsApi(challengeId, configIndex);
         } else {
             mApiModelListener.onNoInternet();
         }
     }
 
-    private void callConfigsApi(int challengeId,Integer configIndex) {
+    private void callConfigsApi(int challengeId, Integer configIndex) {
+
         mApiModelListener.onApiCallStarted();
 
-        MyWebService.getInstance().getChallengeConfigsRequest(challengeId,configIndex).enqueue(
+        //Log.d("appflavor", getAppFlavor());
+
+        MyWebService.getInstance().getChallengeConfigsRequest(challengeId, configIndex).enqueue(
+
                 new NostragamusCallBack<ChallengeConfigsResponse>() {
                     @Override
                     public void onResponse(Call<ChallengeConfigsResponse> call, Response<ChallengeConfigsResponse> response) {
@@ -86,6 +93,38 @@ public class ChallengeConfigsApiModelImpl {
             ).getConfigs();
         }
         return null;
+    }
+
+    public String getAppFlavor() {
+
+        String appFlavor ="";
+        switch (BuildConfig.FLAVOR) {
+            case "productionPaidRelease":
+                appFlavor = "productionPaidRelease";
+                break;
+            case "productionRelease":
+                appFlavor = "productionRelease";
+                break;
+            case "stagePaidRelease":
+                appFlavor = "stagePaidRelease";
+                break;
+            case "stageRelease":
+                appFlavor = "stageRelease";
+                break;
+            case "stagePaidDebug":
+                appFlavor = "stagePaidDebug";
+                break;
+            case "stageDebug":
+                appFlavor = "stageDebug";
+                break;
+            case "prodDebug":
+                appFlavor = "prodDebug";
+                break;
+            case "prodPaidDebug":
+                appFlavor = "prodPaidDebug";
+                break;
+        }
+        return appFlavor;
     }
 
     public interface OnConfigsApiModelListener {
