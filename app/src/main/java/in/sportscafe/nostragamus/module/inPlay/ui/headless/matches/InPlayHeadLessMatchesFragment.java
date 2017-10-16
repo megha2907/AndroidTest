27a2abf6220d4b15e1583e88ab8d95bbbdf4cf6d
shenoy <vigneshshenoy@gmail.com>
2017-10-16 18:46:46 +0530

    text changes

--- app/src/main/java/in/sportscafe/nostragamus/module/inPlay/ui/headless/matches/InPlayHeadLessMatchesFragment.java
+++ app/src/main/java/in/sportscafe/nostragamus/module/inPlay/ui/headless/matches/InPlayHeadLessMatchesFragment.java
@@ -367,7 +367,7 @@ public class InPlayHeadLessMatchesFragment extends BaseFragment implements View.
                 if (match.getMatchStatus().equalsIgnoreCase(Constants.MatchStatusStrings.ANSWER) ||
                         match.getMatchStatus().equalsIgnoreCase(Constants.MatchStatusStrings.CONTINUE)) {
 
-                    String msg = "Your predictions were saved. You can edit or play the challenge with them, once you join, before the timer runs out!";
+                    String msg = "But you need to join a contest before any match starts, to continue playing this challenge.";
                     infoMsgTextView.setText(msg);
                     break;
                 }
